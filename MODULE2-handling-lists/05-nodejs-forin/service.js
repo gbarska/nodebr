c19d2d76f0b73b52bcc459ad50df940ddff5c42f
Gustavo B Barska <gbarska@yahoo.com>
2020-02-02 21:37:05 -0200

    renamed module1 and added first files for module2

---
+++ MODULE2-handling-lists/05-nodejs-forin/service.js
@@ -0,0 +1,16 @@
+const axios = require('axios');
+const URL = `https://swapi.co/api/people`;
+
+async function getPeople(name){
+    const url = `${URL}/?search=${name}&format=json`
+    const response = await axios.get(url)
+    return response.data;
+}
+
+// getPeople('r2')
+//     .then(function (result){
+//         console.log('result',result);
+//     })
+//     .catch(function (error){
+//         console.log('There was an error', error);
+//     })
